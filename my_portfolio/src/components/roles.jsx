03f97db963f53559ff619df37b61c5ba7e17c3ac
Esther Kariuki <kariukie@whitman.edu>
2025-12-27 15:33:08 -0800

    27/12 update

--- my_portfolio/src/components/roles.jsx
+++ my_portfolio/src/components/roles.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import '../styles/roles.css'
 import daytona4 from '../assets/Daytona4.jpg'
 import devfest from '../assets/devfest1.jpg'
+import meonmic from '../assets/IMG-20240330-WA0001.jpg'
 import Title from './Title'
 //import daytona2 from '../assets/Daytona2.jpg'
 //import daytona3 from '../assets/Daytona3.jpg' 
@@ -10,7 +11,30 @@ export default function Roles() {
     <>
     <Title name ={'My roles'}/>
     <div className="roles">
-      
+      <div className="role">
+        <h1>Student Fellow, Shamiri Institute</h1>
+        <h2>September 2025 - Present</h2>
+        <p>
+         Provided peer-based support using digital mental wellness tools   
+         Used data management platforms to communicate with students and manage their records 
+        </p>
+        <div className="gallery">
+          <img src = {meonmic} alt = 'daytona4' className='img1'></img>
+        </div>
+      </div>
+
+      <div className="role">
+        <h1>Student Fellow, Shamiri Institute</h1>
+        <h2>May 2025 - August 2025</h2>
+        <p>
+         Provided peer-based support using digital mental wellness tools   
+         Used data management platforms to communicate with students and manage their records 
+        </p>
+        <div className="gallery">
+          <img src = {meonmic} alt = 'daytona4' className='img1'></img>
+        </div>
+      </div>
+
       <div className="role">
         <h1>Administrative assistant, Kenya Tech Events</h1>
         <h2>June 2024 - February 2025</h2>
